00bf188f83b2044f2a70029af98bf4a8a4837004
tminhat13 <83794482+tminhat13@users.noreply.github.com>
2023-01-07 11:42:37 -0800

    ini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningAlert } from './warning/warning.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SuccessComponent, WarningAlert],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './warning.component.html',
+  styleUrls: ['./warning.component.css'],
+})
+export class WarningAlert {}
